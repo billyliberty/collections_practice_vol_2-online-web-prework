bc00e937432450d3dd48f6bbf28a9e46906c9ab2
Joshua Crum <joshua.crum@gmail.com>
2019-05-11 21:40:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,6 @@ def begins_with_r (tools)
   tools.all? { |word| word.start_with?("r") }
 end
 
-def contain_letter_a(elements)
+def contain_a(elements)
   elements.select { |word| word.include?("a") }
 end
